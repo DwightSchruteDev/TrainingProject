cc490667c913d3006430c946f33a1b43c6647e92
Dwight Schrute <DwightSchruteDev@gmail.com>
2023-10-02 15:15:46 +0300

    Training Day 5 - While

---
+++ CodeBlocks/CTraining/while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int count = 1;
+    while(count<=5)
+    {
+        printf("row %d", count);
+        count++;
+    }
+    /* printf("Enter a number please: ");
+    int number;
+    scanf("%d", &number);
+
+    while(number != -1)
+    {
+        int square = number * number;
+        printf("\nNumber of %d square: %d", number, square);
+        printf("\nEnter a number please: ");
+        scanf("%d", &number);
+    } */
+}
